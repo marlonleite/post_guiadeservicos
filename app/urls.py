6d4494e0f23271a7553778e611a2325264cf31d3
Marlon Leite <marlonleite@gmail.com>
2018-03-06 14:18:53 -0300

    psycopg

--- app/urls.py
+++ app/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('app.api.urls', namespace='api')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
